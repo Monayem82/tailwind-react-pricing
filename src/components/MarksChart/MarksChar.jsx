84a7619b486607274b34ac6dab7cae45060e1585
Monayem <mamonayem6@gmail.com>
2025-11-02 00:13:24 +0600

    chart and recharts

--- src/components/MarksChart/MarksChar.jsx
+++ src/components/MarksChart/MarksChar.jsx
@@ -1,25 +1,25 @@
 import React, { use } from 'react';
-import { Bar, BarChart, Tooltip, XAxis, YAxis } from 'recharts';
+import { Bar, BarChart, Pie, PieChart, Tooltip, XAxis, YAxis } from 'recharts';
 
-const MarksChar = ({marksChartpromize}) => {
-    const marksDataResponse=use(marksChartpromize)
+const MarksChar = ({ marksChartpromize }) => {
+    const marksDataResponse = use(marksChartpromize)
     // console.log(marksDataResponse)
 
     // Data preprocessing for chart
-    const markDataRes=marksDataResponse.data
+    const markDataRes = marksDataResponse.data
 
-    const marksData=markDataRes.map((students)=>{
-        const student={
-            id:students.ID,
-            name:students.Name,
-            physics:students.Marks.Physics,
-            math:students.Marks.Math,
-            chemistry:students.Marks.Chemistry,
+    const marksData = markDataRes.map((students) => {
+        const student = {
+            id: students.ID,
+            name: students.Name,
+            physics: students.Marks.Physics,
+            math: students.Marks.Math,
+            chemistry: students.Marks.Chemistry,
         }
         // student.avg=(physics+math+chemistry)/3
-        const avg=(student.chemistry+student.math+student.physics)/3
-        student.avg=avg
-        
+        const avg = (student.chemistry + student.math + student.physics) / 3
+        student.avg = avg
+
         return student
     })
 
@@ -36,6 +36,20 @@ const MarksChar = ({marksChartpromize}) => {
                 <Bar dataKey={'chemistry'} fill='#8884d8'></Bar>
 
             </BarChart>
+            <PieChart data={marksData} style={{ width: '100%', maxWidth: '500px', maxHeight: '80vh', aspectRatio: 1 }}>
+                <XAxis dataKey={'name'}></XAxis>
+                <Tooltip></Tooltip>
+                <Pie
+                    dataKey="math"
+                    nameKey="name"
+                    cx="50%"
+                    cy="50%"
+                    outerRadius="50%"
+                    fill="#8884d8"
+                />
+
+            </PieChart>
+
         </div>
     );
 };
